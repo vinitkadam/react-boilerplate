6bef10150b476788392f2e922641028889c58f9e
Rohan Kamat <rohan.ka@media.net>
2017-10-05 21:17:02 +0530

    Remove babelrc and app.js

--- app/components/Button/index.js
+++ app/components/Button/index.js
@@ -1,4 +1,4 @@
-import React form 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 function Button({children, ...rest}) {
--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -2,28 +2,28 @@
  *
  * App.js
  *
- * This component is the skeleton around the actual pages, and should only
- * contain code that should be seen on all pages. (e.g. navigation bar)
- *
- * NOTE: while this component should technically be a stateless functional
- * component (SFC), hot reloading does not currently support SFCs. If hot
- * reloading is not a necessity for you then you can refactor it and remove
- * the linting exception.
  */
 
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Switch, Route } from 'react-router-dom';
 
 import HomePage from 'containers/HomePage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
 
-export default function App() {
+function App({ match }) {
   return (
     <div>
       <Switch>
-        <Route exact path="/" component={HomePage} />
+        <Route exact path={match.url} component={HomePage} />
         <Route component={NotFoundPage} />
       </Switch>
     </div>
   );
 }
+
+App.propTypes = {
+  match: PropTypes.object.isRequired,
+};
+
+export default App;
