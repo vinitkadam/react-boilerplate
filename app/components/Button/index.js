f9007fd28b9316abd2862b0c205364e296e6367a
Sabarni Das <sabarnix@gmail.com>
2020-03-18 02:26:39 +0530

    fixed eslint

--- app/components/Button/index.js
+++ app/components/Button/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Button({ children, ...rest }) {
-  return <button {...rest}> {children} </button>;
+  return (
+    <button type="button" {...rest}>
+      {' '}
+      {children}
+      {' '}
+    </button>
+  );
 }
 
 Button.propTypes = {
